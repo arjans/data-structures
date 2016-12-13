aad35d1a2ea6de7df5017a89b365fffe2a379100
Arjan Singh <arjansingh1989@gmail.com>
2016-12-13 13:54:24 -0500

    Custom match syntax for < and >.

--- red-black-tree.rkt
+++ red-black-tree.rkt
@@ -9,84 +9,103 @@
       y
       (x (car z) (reduce x y (cdr z)))))
 
+(define-match-expander
+  <<
+  (lambda (stx)
+    (syntax-case stx ()
+      [(_ val) #'(? (lambda (x) (< x val)))])))
+
+(define-match-expander
+  >>
+  (lambda (stx)
+    (syntax-case stx ()
+      [(_ val) #'(? (lambda (x) (> x val)))])))
+
+;(define-match-expander <<
+;  (lambda (val)
+;    (syntax-case val ()
+;      [y #'(? (lambda (x) (< x val)) y)])))
+
 ; Colors don't make as much sense when you add double-black and negative black.
 ; I'm using numbers here instead. Black is 1, red is 0, negative black is -1, double black is 2.
 ; The idea is to keep black height constant. Numbers make that relationship apparent.
 
-(struct tree (color left value right))
+; Pattern matching means no type errors compared with nested conditionals.
+
+(struct T (color left value right))
 
 (define (min t)
-  (if (tree-left t)
-      (min (tree-left t))
-      (tree-value t)))
+  (if (T-left t)
+      (min (T-left t))
+      (T-value t)))
 
 (define (rb-insert v t)
   ; helper function for recursion
   (define (helper x y)
     (if y
-        (match-let ([(tree tc tl tv tr) y])
-          (cond ((< x tv) (balance (tree tc (helper x tl) tv tr)))
-                ((> x tv) (balance (tree tc tl tv (helper x tr))))
+        (match-let ([(T tc tl tv tr) y])
+          (cond ((< x tv) (balance (T tc (helper x tl) tv tr)))
+                ((> x tv) (balance (T tc tl tv (helper x tr))))
                 (else y)))
-        (tree 0 #f x #f)))
+        (T 0 #f x #f)))
   ; call helper function, then re-color root node to black
-  (match-let ([(tree _ x y z) (helper v t)])
-    (tree 1 x y z)))
+  (match-let ([(T _ x y z) (helper v t)])
+    (T 1 x y z)))
 
 (define (rb-delete v t)
   (define (helper x y)
-    (define (less-than-target? v)
-      (< v x))
-    (define (greater-than-target? v)
-      (> v x))
     (match y
       ; value not found
-      [#f #f]
+      [#f                                         #f]
       ; red leaf node
-      [(tree 0 #f (== x) #f) #f]
+      [(T 0 #f (== x) #f)                         #f]
       ; target node has only one branch
-      [(or (tree c (tree _ bl bv br) (== x) #f) (tree c #f (== x) (tree _ bl bv br))) (tree c bl bv br)]
+      [(or (T c (T _ bl bv br) (== x) #f)
+           (T c #f (== x) (T _ bl bv br)))        (T c bl bv br)]
       ; target node has two branches
-      [(tree c (? tree? l) (== x) (? tree? r)) (bubble (tree c l (min r) (helper (min r) r)))]
+      [(T c (? T? l) (== x) (? T? r))             (bubble (T c l (min r) (helper (min r) r)))]
       ; leaf black child node
-      [(tree c (tree 1 #f (== x) #f) v (tree _ rl rv rr)) (bubble (tree (+ 1 c) #f v (tree 0 rl rv rr)))]
-      [(tree c (tree _ ll lv lr) v (tree 1 #f (== x) #f)) (bubble (tree (+ 1 c) (tree 0 ll lv lr) v #f))]
+      [(T c (T 1 #f (== x) #f) v (T _ rl rv rr))  (bubble (T (+ 1 c) #f v (T 0 rl rv rr)))]
+      [(T c (T _ ll lv lr) v (T 1 #f (== x) #f))  (bubble (T (+ 1 c) (T 0 ll lv lr) v #f))]
       ; node value is less than target value
-      [(tree c l (? less-than-target? v) r) (bubble (tree c l v (helper x r)))]
+      [(T c l (<< x) r)                           (bubble (T c l (T-value t) (helper x r)))]
       ; node value is greater than target value
-      [(tree c l (? greater-than-target? v) r) (bubble (tree c (helper x l) v r))]
+      [(T c l (>> x) r)                           (bubble (T c (helper x l) (T-value t) r))]
       ; this case covers root deletion
-      [(tree _ #f (== x) #f) #f]
-      [_ 'error-in-red-black-delete]))
+      [(T _ #f (== x) #f)                         #f]
+      [_                                          'error-in-red-black-delete]))
   (match (helper v t)
-    [(tree _ x y z) (tree 1 x y z)]
+    [(T _ x y z) (T 1 x y z)]
     [#f #f]))
 
 (define (bubble t)
   (match t
-    [(or (tree (or 1 0) (tree 2 ll lv lr) v (tree _ rl rv rr))
-         (tree (or 1 0) (tree _ ll lv lr) v (tree 2 rl rv rr)))
-     (balance (tree (+ 1 (tree-color t))
-                    (tree (- (tree-color (tree-left t)) 1) ll lv lr)
+    ; red or black parent with a double-black child
+    [(or (T (or 1 0) (T 2 ll lv lr) v (T _ rl rv rr))
+         (T (or 1 0) (T _ ll lv lr) v (T 2 rl rv rr)))
+    ; return:
+     (balance (T (+ 1 (T-color t))
+                    (T (- (T-color (T-left t)) 1) ll lv lr)
                     v
-                    (tree (- (tree-color (tree-right t)) 1) rl rv rr)))]
+                    (T (- (T-color (T-right t)) 1) rl rv rr)))]
+    ; skip other subTs
     [_ t]))
 
 (define (balance t)
   (match t
     ; Match black or double-black with two red descendants
-    [(or (tree (or 1 2) (tree 0 (tree 0 a x b) y c) z d)
-         (tree (or 1 2) a x (tree 0 b y (tree 0 c z d)))
-         (tree (or 1 2) a x (tree 0 (tree 0 b y c) z d))
-         (tree (or 1 2) (tree 0 a x (tree 0 b y c)) z d))
+    [(or (T (or 1 2) (T 0 (T 0 a x b) y c) z d)
+         (T (or 1 2) a x (T 0 b y (T 0 c z d)))
+         (T (or 1 2) a x (T 0 (T 0 b y c) z d))
+         (T (or 1 2) (T 0 a x (T 0 b y c)) z d))
      ; return:
-     (tree (- (tree-color t) 1) (tree 1 a x b) y (tree 1 c z d))]
+     (T (- (T-color t) 1) (T 1 a x b) y (T 1 c z d))]
     
     ; Match a negative black child
-    [(or (tree 2 (tree -1 (tree _ a w b) x (tree _ c y d)) z e)
-         (tree 2 a w (tree -1 (tree _ b x c) y (tree _ d z e))))
+    [(or (T 2 (T -1 (T _ a w b) x (T _ c y d)) z e)
+         (T 2 a w (T -1 (T _ b x c) y (T _ d z e))))
      ; return:
-     (tree 1 (tree 1 (tree 0 a w b) x c) y (tree 1 d z e))]
+     (T 1 (T 1 (T 0 a w b) x c) y (T 1 d z e))]
     
     ; Skip everything else
     [_ t]))
@@ -94,11 +113,11 @@
 (define (display-tree t)
   (define (layout t)
     (match t
-      [(tree c l v r)
+      [(T c l v r)
        (tree-layout
         #:pict (cc-superimpose (text (number->string v))
                                (circle 30 #:border-color (match c [0 "red"] [1 "black"] [2 "blue"] [-1 "yellow"])))
         (layout l)
         (layout r))]
-      [_ t]))
+      [#f #f]))
   (binary-tidier (layout t)))
